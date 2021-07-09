a71f5d739687d24c1e9bf0889e85ac4fdc5f130a
MarlenySosa <marrlensosa@gmail.com>
2021-07-09 13:01:05 -0600

    crud usuario

---
+++ controller/user.controller.php
@@ -0,0 +1,52 @@
+<?php
+require_once 'model/user.model.php';
+
+class UserController{
+    
+    private $model;
+    private $controller;
+
+    public function __CONSTRUCT(){
+        $this->model = new User();
+        
+    }
+    
+    public function Index(){
+        require_once 'view/header.php';
+        require_once 'view/user/user.php';
+        require_once 'view/footer.php';
+    }   
+
+    public function Crud(){
+        $tea = new User();
+        if(isset($_REQUEST['id'])){
+            $tea = $this->model->GetID($_REQUEST['id']);
+        }
+        require_once 'view/header.php';
+        require_once 'view/user/edit.php';
+        require_once 'view/footer.php';
+    }
+    
+    public function Load(){
+        $tea = new User();
+        
+        $tea->id = $_REQUEST['id'];
+        $tea->name = $_REQUEST['name'];
+        $tea->user = $_REQUEST['user'];
+        $tea->password = $_REQUEST['password'];
+        $tea->type = $_REQUEST['type'];
+        $tea->address = $_REQUEST['address'];
+        $tea->phone = $_REQUEST['phone'];
+
+        $tea->id > 0 
+            ? $this->model->Update($tea)
+            : $this->model->Register($tea);
+        
+        header('Location: index.php');
+    }
+
+    public function Delete(){
+        $this->model->Delete($_REQUEST['id']);
+        header('Location:index.php');
+    }
+}
